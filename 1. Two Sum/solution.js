129783bafa6fcf88066bdefacf0bdbfb727bc8d4
Douglas Garcia <douglas.garcia.shaevka@gmail.com>
2022-09-06 19:57:41 -0400

    two sum solution

--- 1. Two Sum/solution.js
+++ 1. Two Sum/solution.js
@@ -3,6 +3,21 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    
-};
+
+nums = [3, 2, 4];
+target = 6;
+
+function main (nums, target) {
+    for (var i = 0; i < nums.length; i++) {
+        for (var j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [i, j];
+            }
+        }
+    }
+}
+
+var twoSum = main(nums, target);
+
+console.log(twoSum);
+
